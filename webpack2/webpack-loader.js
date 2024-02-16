8fe32db76de287c66ecbee746197c388318c5845
DowonKim <kimdowon1404@gmail.com>
2024-02-16 16:26:48 +0900

    feat : loader 학습 진행 중

---
+++ webpack2/webpack-loader.js
@@ -0,0 +1,4 @@
+module.exports = function myLoader(data) {
+  console.log("로더 확인함");
+  return data;
+};
--- webpack2/webpack.config.js
+++ webpack2/webpack.config.js
@@ -1,10 +1,18 @@
-let path = require("path");
+const path = require("path");
 
 module.exports = {
-  mode: "none",
+  mode: "development",
   entry: "./src/index.js",
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/, //loader가 처리할 패턴
+        use: [path.resolve("./webpack-loader.js")],
+      },
+    ],
   },
 };
