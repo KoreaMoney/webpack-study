e063ea2b133613d8a563299af2a2c1a5e6d787fb
DowonKim <kimdowon1404@gmail.com>
2024-02-19 10:54:33 +0900

    Feat : loader내용 추가

---
+++ webpack2/src/index.js
@@ -0,0 +1,6 @@
+import "../src/index.css";
+import test from "./test.png";
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.body.innerHTML = `<img src="${test}" />`;
+});
--- webpack2/webpack.config.js
+++ webpack2/webpack.config.js
@@ -4,15 +4,44 @@ module.exports = {
   mode: "development",
   entry: "./src/index.js",
   output: {
+    path: path.resolve(__dirname, "./dist"),
+    publicPath: "/dist/", // 경로
     filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
   },
   module: {
     rules: [
       {
-        test: /\.js$/, //loader가 처리할 패턴
-        use: [path.resolve("./webpack-loader.js")],
+        test: /\.css$/, // css파일 인식
+        use: ["style-loader", "css-loader"], // css파일 로드, style먼저쓰고 css를 써야 한다. 순서가 중요하다.
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/, // image 속성 인식
+        loader: "url-loader", // image파일 로드
+        options: {
+          publicPath: "./dist/",
+          name: "[name].[ext]?[hash]", // image파일 상태(이름.확장자, hash)
+          limit: 20000, // 20kb
+        },
       },
     ],
   },
 };
+
+//  module: {
+//   rules: [
+//     {
+//       test: /\.js$/, //loader가 처리할 패턴
+//       use: [path.resolve("./webpack-loader.js")],
+//     },
+//   ],
+// },
+
+// IMAGE처리하는 파일 로더 방식
+// {
+//   test: /\.(png|jpg|gif|svg)$/, // image 속성 인식
+//   loader: "file-loader", // image파일 로드
+//   options: {
+//     publicPath: "./dist/",
+//     name: "[name].[ext]?[hash]", // image파일 상태(이름.확장자, hash)
+//   },
+// },
